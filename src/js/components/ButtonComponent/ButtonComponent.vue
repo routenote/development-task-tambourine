dab140157192bc7b27ee8eadc0b68cdd071a04c3
oluwaseun-gbenle <pinkpearl3010@gmail.com>
2024-01-02 20:59:50 +0100

    addition of dropdown button component

---
+++ src/js/components/ButtonComponent/ButtonComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="dropdown-button" :class="{'disabled':buttonDisabled,'z-50':isDropdownVisible}" :disabled="buttonDisabled">
+    <img v-if="viewIcon" class="button-svg"  :src="fillerSvgUrl" alt="filler-svg" />
+    Button
+    <img class="button-svg" :class="{ 'rotate-svg': isDropdownVisible }" :src="arrowSvgUrl" alt="arrow-svg" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonComponent",
+  props: {
+    isDropdownVisible: Boolean,
+    viewIcon: Boolean,
+    buttonDisabled: Boolean
+  },
+  data() {
+    return {
+      fillerSvgUrl: "/filler.svg",
+      arrowSvgUrl: "/icon-arrow.svg",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../../css/tailwind';
+</style>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 module.exports = {
-    future: {},
-    purge: {
-      mode: 'jit',
-      content: [
-        'dist/**/*.html',
-        'src/**/*.vue',
-      ],
-      options: {},
+  future: {},
+  purge: {
+    mode: "jit",
+    content: ["dist/**/*.html", "src/**/*.vue"],
+    options: {},
+  },
+  theme: {
+    screens: {
+      'sm': '540px',
+      'md': '720px',
+      'lg': '960px',
+      'xl': '1140px',
     },
-    theme: {},
-    plugins: [],
-    prefix: '',
-  }
+    extend: {
+      colors: {
+        "mid-grey-opacity-12": "rgba(174, 174, 186, 0.12)",
+        "dark-grey": "#9898a3",
+        "mid-grey": "#aeaeba",
+        charcoal: "#202020",
+      },
+    },
+  },
+  plugins: [],
+  prefix: "",
+};
